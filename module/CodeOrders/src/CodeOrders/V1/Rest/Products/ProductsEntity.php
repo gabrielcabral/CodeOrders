84d770d2f9f747615d10a30209ab91bc99f3dcec
Candido Souza <candido_souzza@hotmail.com>
2015-09-01 11:56:47 -0300

    Criando Entidade Products

--- module/CodeOrders/src/CodeOrders/V1/Rest/Products/ProductsEntity.php
+++ module/CodeOrders/src/CodeOrders/V1/Rest/Products/ProductsEntity.php
@@ -1,6 +1,119 @@
 <?php
 namespace CodeOrders\V1\Rest\Products;
 
+use Zend\Stdlib\Hydrator;
+
 class ProductsEntity
 {
+    /**
+     * @var integer
+     */
+    protected $id;
+
+    /**
+     * @var string
+     */
+    protected $name;
+
+    /**
+     * @var string
+     */
+    protected $description;
+
+    /**
+     * @var
+     */
+    protected $price;
+
+    /**
+     * Construct
+     *
+     * @param array $options
+     */
+    public function __construct(array $options = [])
+    {
+        (new Hydrator\ClassMethods())->hydrate($options, $this);
+    }
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     * @return ProductsEntity
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     * @return ProductsEntity
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param string $description
+     * @return ProductsEntity
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPrice()
+    {
+        return $this->price;
+    }
+
+    /**
+     * @param mixed $price
+     * @return ProductsEntity
+     */
+    public function setPrice($price)
+    {
+        $this->price = $price;
+        return $this;
+    }
+
+    /**
+     * ToArray
+     *
+     * @return array
+     */
+    public function toArray()
+    {
+        return (new Hydrator\ClassMethods())->extract($this);
+    }
 }
