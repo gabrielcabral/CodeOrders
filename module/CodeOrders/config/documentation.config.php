5c14028ebfc2541fe992295db6cdea3142e547ca
Candido Souza <candido_souzza@hotmail.com>
2015-09-01 10:53:41 -0300

    Criando ProductsRepository

--- module/CodeOrders/config/documentation.config.php
+++ module/CodeOrders/config/documentation.config.php
@@ -330,8 +330,123 @@ return array(
             ),
         ),
     ),
+    'CodeOrders\\V1\\Rest\\Ptypes\\Controller' => array(
+        'description' => 'Tipos de pagamentos | Handles payments types',
+        'collection' => array(
+            'description' => 'Coleção de Tipos de Pagamentos | Collection PaymentsTypes',
+            'GET' => array(
+                'description' => 'Lista todos os tipos de pagamentos | List all payment types',
+                'response' => '{
+   "_links": {
+       "self": {
+           "href": "/ptypes"
+       },
+       "first": {
+           "href": "/ptypes?page={page}"
+       },
+       "prev": {
+           "href": "/ptypes?page={page}"
+       },
+       "next": {
+           "href": "/ptypes?page={page}"
+       },
+       "last": {
+           "href": "/ptypes?page={page}"
+       }
+   }
+   "_embedded": {
+       "ptypes": [
+           {
+               "_links": {
+                   "self": {
+                       "href": "/ptypes[/:ptypes_id]"
+                   }
+               }
+              "name": "Nome do tipo de pagamento | Name of playment type"
+           }
+       ]
+   }
+}',
+            ),
+            'POST' => array(
+                'description' => 'Criar um novo tipo de pagamento | Create a new payment type',
+                'request' => '{
+   "name": "Nome do tipo de pagamento | Name of playment type"
+}',
+                'response' => '{
+   "_links": {
+       "self": {
+           "href": "/ptypes[/:ptypes_id]"
+       }
+   }
+   "id": "ID do tipo de pagamento | ID of payment type"
+   "name": "Nome do tipo de pagamento | Name of playment type"
+}',
+            ),
+        ),
+        'entity' => array(
+            'description' => 'Entidade PaymentType | PaymentType Entity',
+            'GET' => array(
+                'description' => 'Retorna um tipo de pagamento | Returns a payment type',
+                'response' => '{
+   "_links": {
+       "self": {
+           "href": "/ptypes[/:ptypes_id]"
+       }
+   }
+   "id": "ID do tipo de pagamento | ID of payment type"
+   "name": "Nome do tipo de pagamento | Name of playment type"
+}',
+            ),
+            'PATCH' => array(
+                'description' => 'Atualiza parcialmente um tipo de pagamento | Update partially a payment type',
+                'request' => '{
+   "name": "Nome do tipo de pagamento | Name of playment type"
+}',
+                'response' => '{
+   "_links": {
+       "self": {
+           "href": "/ptypes[/:ptypes_id]"
+       }
+   }
+    "id": "ID do tipo de pagamento | ID of payment type"
+    "name": "Nome do tipo de pagamento | Name of playment type"
+}',
+            ),
+            'PUT' => array(
+                'description' => 'Atualiza um tipo de pagamento | Update a payment type',
+                'request' => '{
+   "name": "Nome do tipo de pagamento | Name of playment type"
+}',
+                'response' => '{
+   "_links": {
+       "self": {
+           "href": "/ptypes[/:ptypes_id]"
+       }
+   }
+   "id": "ID do tipo de pagamento | ID of payment type"
+   "name": "Nome do tipo de pagamento | Name of playment type"
+}',
+            ),
+            'DELETE' => array(
+                'description' => 'Deleta um tipo de pagamento | Delete a payment type',
+                'request' => '{
+   "name": "Nome do tipo de pagamento | Name of playment type"
+}',
+                'response' => '{
+   "_links": {
+       "self": {
+           "href": "/ptypes[/:ptypes_id]"
+       }
+   }
+    "id": "ID do tipo de pagamento | ID of payment type"
+   "name": "Nome do tipo de pagamento | Name of playment type"
+}',
+            ),
+        ),
+    ),
     'CodeOrders\\V1\\Rest\\Users\\Controller' => array(
-        'description' => 'Usuários do sistema | Systems users',
+        'description' => 'Cadastro de usuários | Users register',
         'collection' => array(
             'description' => 'Usuários cadastrados no sistemas | Users registered systems',
             'GET' => array(
@@ -458,121 +573,6 @@ return array(
 }',
             ),
             'description' => 'Entidade users | Users entity',
-        ),
-    ),
-    'CodeOrders\\V1\\Rest\\Ptypes\\Controller' => array(
-        'description' => 'Tipos de pagamentos | Handles payments types',
-        'collection' => array(
-            'description' => 'Coleção de Tipos de Pagamentos | Collection PaymentsTypes',
-            'GET' => array(
-                'description' => 'Lista todos os tipos de pagamentos | List all payment types',
-                'response' => '{
-   "_links": {
-       "self": {
-           "href": "/ptypes"
-       },
-       "first": {
-           "href": "/ptypes?page={page}"
-       },
-       "prev": {
-           "href": "/ptypes?page={page}"
-       },
-       "next": {
-           "href": "/ptypes?page={page}"
-       },
-       "last": {
-           "href": "/ptypes?page={page}"
-       }
-   }
-   "_embedded": {
-       "ptypes": [
-           {
-               "_links": {
-                   "self": {
-                       "href": "/ptypes[/:ptypes_id]"
-                   }
-               }
-              "name": "Nome do tipo de pagamento | Name of playment type"
-           }
-       ]
-   }
-}',
-            ),
-            'POST' => array(
-                'description' => 'Criar um novo tipo de pagamento | Create a new payment type',
-                'request' => '{
-   "name": "Nome do tipo de pagamento | Name of playment type"
-}',
-                'response' => '{
-   "_links": {
-       "self": {
-           "href": "/ptypes[/:ptypes_id]"
-       }
-   }
-   "id": "ID do tipo de pagamento | ID of payment type"
-   "name": "Nome do tipo de pagamento | Name of playment type"
-}',
-            ),
-        ),
-        'entity' => array(
-            'description' => 'Entidade PaymentType | PaymentType Entity',
-            'GET' => array(
-                'description' => 'Retorna um tipo de pagamento | Returns a payment type',
-                'response' => '{
-   "_links": {
-       "self": {
-           "href": "/ptypes[/:ptypes_id]"
-       }
-   }
-   "id": "ID do tipo de pagamento | ID of payment type"
-   "name": "Nome do tipo de pagamento | Name of playment type"
-}',
-            ),
-            'PATCH' => array(
-                'description' => 'Atualiza parcialmente um tipo de pagamento | Update partially a payment type',
-                'request' => '{
-   "name": "Nome do tipo de pagamento | Name of playment type"
-}',
-                'response' => '{
-   "_links": {
-       "self": {
-           "href": "/ptypes[/:ptypes_id]"
-       }
-   }
-    "id": "ID do tipo de pagamento | ID of payment type"
-    "name": "Nome do tipo de pagamento | Name of playment type"
-}',
-            ),
-            'PUT' => array(
-                'description' => 'Atualiza um tipo de pagamento | Update a payment type',
-                'request' => '{
-   "name": "Nome do tipo de pagamento | Name of playment type"
-}',
-                'response' => '{
-   "_links": {
-       "self": {
-           "href": "/ptypes[/:ptypes_id]"
-       }
-   }
-   "id": "ID do tipo de pagamento | ID of payment type"
-   "name": "Nome do tipo de pagamento | Name of playment type"
-}',
-            ),
-            'DELETE' => array(
-                'description' => 'Deleta um tipo de pagamento | Delete a payment type',
-                'request' => '{
-   "name": "Nome do tipo de pagamento | Name of playment type"
-}',
-                'response' => '{
-   "_links": {
-       "self": {
-           "href": "/ptypes[/:ptypes_id]"
-       }
-   }
-    "id": "ID do tipo de pagamento | ID of payment type"
-   "name": "Nome do tipo de pagamento | Name of playment type"
-}',
-            ),
         ),
     ),
 );
---
+++ module/CodeOrders/src/CodeOrders/V1/Rest/Products/Repository/ProductsRepositoty.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace CodeOrders\V1\Rest\Products\Repository;
+
+use CodeOrders\V1\Rest\Base\Repository\AbstractRepository;
+use Zend\Db\TableGateway\TableGatewayInterface;
+
+/**
+ * Class ProductsRepositoty
+ * @package CodeOrders\V1\Rest\Products\Repository
+ */
+class ProductsRepositoty extends AbstractRepository
+{
+    /**
+     * @var TableGatewayInterface
+     */
+    protected $tableGateway;
+
+    /**
+     * Construct
+     *
+     * @param TableGatewayInterface $tableGateway
+     */
+    public function __construct(TableGatewayInterface $tableGateway)
+    {
+        $this->tableGateway = $tableGateway;
+        parent::__construct($this->tableGateway);
+
+        $this->collection = 'CodeOrders\\V1\\Rest\\Products\\ProductsCollection';
+    }
+}
